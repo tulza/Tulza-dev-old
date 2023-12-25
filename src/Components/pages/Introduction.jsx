051866f577d4daa306b221bd20c80082fe89b400
Tulza <40633228+tulza@users.noreply.github.com>
2023-12-25 16:42:07 +1300

    completed head

--- src/Components/pages/Introduction.jsx
+++ src/Components/pages/Introduction.jsx
@@ -3,44 +3,58 @@ import { blob } from "../svg/ImageImport.jsx";
 import { easeIn, motion } from "framer-motion";
 import clsx from "clsx";
 
+const A = () => {
+  return (
+    <>
+      {/* this is <a/> */}
+      <motion.span
+        className="white-stroke cursor-pointer text-3xl font-bold text-transparent"
+        whileHover={{ color: "#fff" }}
+      >
+        &lt;a/&zwnj;&gt;
+      </motion.span>
+    </>
+  );
+};
+
 const Blobs = () => {
   const rotate = 360;
   const initial = [0, 90, 180];
-  const transition = -45;
+  const transition = "-45%";
   return (
     <>
       <div className="pointer-events-none">
         <motion.img
-          className="absolute left-0 top-0 w-[40%] translate-x-[-50%] translate-y-[-50%] rotate-90"
+          className="absolute left-0 top-0 w-[500px]"
           src={blob}
           alt="spinning blob"
           initial={{
-            x: `${transition}%`,
-            y: `${transition}%`,
+            x: transition,
+            y: transition,
             rotateZ: initial[0],
           }}
           animate={{ rotateZ: initial[0] + rotate }}
           transition={{ ease: "linear", duration: 11, repeat: Infinity }}
         />
         <motion.img
-          className="absolute left-0 top-0 w-[40%] translate-x-[-50%] translate-y-[-50%]"
+          className="absolute left-0 top-0 w-[500px]"
           src={blob}
           alt="spinning blob"
           initial={{
-            x: `${transition}%`,
-            y: `${transition}%`,
+            x: transition,
+            y: transition,
             rotateZ: initial[1],
           }}
           animate={{ rotateZ: initial[1] + rotate }}
           transition={{ ease: "linear", duration: 23, repeat: Infinity }}
         />
         <motion.img
-          className="absolute left-0 top-0 w-[40%] translate-x-[-50%] translate-y-[-50%]"
+          className="absolute left-0 top-0 w-[500px]"
           src={blob}
           alt="spinning blob"
           initial={{
-            x: `${transition}%`,
-            y: `${transition}%`,
+            x: transition,
+            y: transition,
             rotateZ: initial[2],
           }}
           animate={{ rotateZ: initial[2] + rotate }}
@@ -50,31 +64,51 @@ const Blobs = () => {
     </>
   );
 };
-const A = () => {
+
+const CircleLine = () => {
+  const Circle = ({ color }) => {
+    const stroke = `outline-${color}`;
+    return (
+      <motion.div
+        className={clsx(
+          "aspect-square w-4 cursor-pointer rounded-full outline outline-1",
+          stroke,
+        )}
+      ></motion.div>
+    );
+  };
   return (
-    <>
-      {/* this is <a/> */}
-      <span className="white-stroke text-3xl font-bold text-transparent">
-        &lt;a/&zwnj;&gt;
-      </span>
-    </>
+    <div className="absolute right-8 top-4 flex w-0 flex-col gap-2">
+      {/* uses tailwind config color */}
+      <Circle color="white" />
+      <Circle color="red" />
+      <Circle color="yellow" />
+      <Circle color="green" />
+      <Circle color="blue" />
+      <Circle color="purple" />
+    </div>
   );
 };
 
 const Navigation = () => {
+  const highlightBox = {
+    hovered: { height: "25%" },
+  };
+
   const Button = ({ title, highlight }) => {
     return (
       <>
         <div>
-          <div className="relative cursor-pointer">
+          <motion.div className="relative cursor-pointer" whileHover="hovered">
             <span className="text-xl">{title}</span>
-            <div
+            <motion.div
               className={clsx(
                 "absolute bottom-[-0.25rem] left-[0.5rem] -z-10 h-2/3 w-full opacity-40",
                 `${highlight}`,
               )}
+              variants={highlightBox}
             />
-          </div>
+          </motion.div>
         </div>
       </>
     );
@@ -83,7 +117,7 @@ const Navigation = () => {
     <>
       <div className="flex justify-between p-4">
         <A />
-        <div className="mr-8 flex gap-12">
+        <div className="mr-12 flex gap-12">
           <Button title="About me" highlight="bg-blue" />
           <Button title="Education" highlight="bg-yellow" />
           <Button title="Projects" highlight="bg-green" />
@@ -97,8 +131,12 @@ const Navigation = () => {
 const Introduction = () => {
   return (
     <div className="relative h-[100vh] w-full">
+      {/* Header */}
       <Blobs />
+      <CircleLine />
       <Navigation />
+      {/* centre */}
+      <div className="bg-white" />
     </div>
   );
 };
