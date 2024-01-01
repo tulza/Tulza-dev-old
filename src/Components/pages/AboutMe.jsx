1fbdbed437b8252393251cce7082d398c52f37ba
Tulza <40633228+tulza@users.noreply.github.com>
2024-01-02 01:57:25 +1300

    about me horizonal scroll

--- src/Components/pages/AboutMe.jsx
+++ src/Components/pages/AboutMe.jsx
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Section from "../Section";
 import HighlightedText from "../highlightedText";
 import VisibleInView from "../VisibleInView";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
+import { useRef } from "react";
 
 const AboutMe = () => {
+  const targetSectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetSectionRef,
+    offset: ["end end", "start start"],
+  });
+
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    console.log("Page scroll: ", latest);
+  });
+
   return (
-    <div className="relative flex h-[200vh] w-full flex-col">
-      <div className="sticky top-0 grid h-[100vh] p-20 ">
-        <VisibleInView amount={1} className="justify-self-end">
-          <HighlightedText
-            text="&lt;About me/&zwnj;&gt;"
-            className="mr-30 text-4xl"
-            color="blue"
-          />
-        </VisibleInView>
+    <>
+      <motion.div
+        className="sticky top-0 h-10 w-full bg-white"
+        style={{ scaleX: scrollYProgress }}
+      />
+      <div
+        className="relative flex h-[200vh] w-full flex-col"
+        ref={targetSectionRef}
+      >
+        <div className="sticky top-0 grid h-[100vh] p-20 ">
+          <VisibleInView amount={1} className="justify-self-end">
+            <HighlightedText
+              text="&lt;About me/&zwnj;&gt;"
+              className="mr-30 text-4xl"
+              color="blue"
+            />
+          </VisibleInView>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
