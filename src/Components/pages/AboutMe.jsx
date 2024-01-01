38889075b7eeedcf9a2170c4a2404102adb126ea
Tulza <40633228+tulza@users.noreply.github.com>
2024-01-02 03:05:05 +1300

    progression bar fade

--- src/Components/pages/AboutMe.jsx
+++ src/Components/pages/AboutMe.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Section from "../Section";
 import HighlightedText from "../highlightedText";
 import VisibleInView from "../VisibleInView";
@@ -6,37 +6,51 @@ import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 import { useRef } from "react";
 
 const AboutMe = () => {
-  const targetSectionRef = useRef(null);
+  const ref = useRef(null);
   const { scrollYProgress } = useScroll({
-    target: targetSectionRef,
+    target: ref,
     offset: ["end end", "start start"],
-  });
-
-  useMotionValueEvent(scrollYProgress, "change", (latest) => {
-    console.log("Page scroll: ", latest);
   });
 
   return (
     <>
-      <motion.div
-        className="sticky top-0 h-10 w-full bg-white"
-        style={{ scaleX: scrollYProgress }}
-      />
-      <div
-        className="relative flex h-[200vh] w-full flex-col"
-        ref={targetSectionRef}
-      >
-        <div className="sticky top-0 grid h-[100vh] p-20 ">
-          <VisibleInView amount={1} className="justify-self-end">
+      <div className="flex h-[300vh] w-full flex-col" ref={ref}>
+        <div className="sticky top-0 flex h-[100vh] flex-col p-20 ">
+          <VisibleInView amount={1} className="self-end">
             <HighlightedText
               text="&lt;About me/&zwnj;&gt;"
               className="mr-30 text-4xl"
               color="blue"
             />
           </VisibleInView>
+          <ProgressBar scrollYProgress={scrollYProgress} />
         </div>
       </div>
     </>
+  );
+};
+
+const ProgressBar = ({ scrollYProgress }) => {
+  const [visible, setVisible] = useState(false);
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    console.log("Page scroll: ", latest);
+    if (0.9 > latest) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  });
+  return (
+    <motion.div
+      className="absolute bottom-0 left-0 z-10 w-full overflow-hidden"
+      animate={visible ? { opacity: 1 } : { opacity: 0 }}
+      transition={{ duration: 0.25 }}
+    >
+      <motion.div
+        className="h-3 bg-white"
+        style={{ scaleX: scrollYProgress }}
+      />
+    </motion.div>
   );
 };
 
