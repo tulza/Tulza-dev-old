93d2f457fac6151e5a558291ea75faeb2d038cf5
Tulza <40633228+tulza@users.noreply.github.com>
2024-01-06 07:30:57 +1300

    framer exit export and tagscolor to js

---
+++ src/Components/framerExit.js
@@ -0,0 +1,4 @@
+export const slidedownOverflow = {
+  visible: { y: 0, opacity: 1 },
+  hidden: { y: "-100%", opacity: 1 },
+};
---
+++ src/Components/tagsColor.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} tag name of a tag used in the project 
+ * @returns if it is a tag of a tagsColor.jsx returns a color of that tag
+
+ */
+export const getTagColor = (tag) => {
+  tag = String(tag).toLowerCase();
+  switch (tag) {
+    case "react":
+      return "blue"; //blue
+    case "tailwind":
+      return "lightblue"; //lightblue
+    case "html/css":
+      return "red"; //red
+    case "unity":
+    case "framer motion":
+      return "gray"; //gray
+    case "javascript":
+    case "python":
+      return "yellow"; //yellow
+    case "post api":
+    case "rest api":
+    case "turtle graphics":
+      return "green"; //green
+    case "c#":
+    case "responsive":
+      return "purple"; //purple
+
+    default:
+      return "white"; //white
+  }
+};
